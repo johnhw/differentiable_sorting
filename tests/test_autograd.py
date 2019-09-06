3b00c7bfe896360f115942d3e3edb0ea34811a4b
John Williamson <johnhw@gmail.com>
2019-09-06 13:52:18 +0100

    Added vector_sort()

--- tests/test_autograd.py
+++ tests/test_autograd.py
@@ -5,12 +5,24 @@ from differentiable_sorting import (
     bitonic_indices,
     diff_sort_indexed,
     diff_argsort_indexed,
+    vector_sort,
 )
 from differentiable_sorting import softmax, smoothmax, softmax_smooth
 
 maxes = [softmax, smoothmax, softmax_smooth]
 
 np.random.seed(2019)
+
+
+def test_vector_sort():
+    jac_vector_sort = jacobian(vector_sort, argnum=1)
+
+    for n in [2, 4, 8, 16, 32]:
+        matrices = bitonic_matrices(n)
+        for d in [1, 2, 4, 8]:
+            X = np.random.uniform(-100, 100, (d, n))
+            weight = np.random.uniform(0, 1, d)
+            jac = jac_vector_sort(matrices, X, lambda x: (x.T @ weight).T)
 
 
 def test_jacobian_sort():
--- tests/test_differentiable_sort.py
+++ tests/test_differentiable_sort.py
@@ -8,11 +8,39 @@ from differentiable_sorting import (
 from differentiable_sorting import diff_sort_indexed, diff_sort_weave
 from differentiable_sorting import softmax, smoothmax, softmax_smooth
 from differentiable_sorting import diff_sort, diff_argsort, diff_argsort_indexed
+from differentiable_sorting import vector_sort
 
 # non-power-of-2
 # tests for smoothing, woven structure, argsort, etc.
 np.random.seed(2019)
 maxes = [np.maximum, softmax, smoothmax, softmax_smooth]
+
+
+def test_vector_sort():
+    test_array = np.array([[-10, 2, 30, 4, 5, 6, 7, 80], [5, 6, 7, 8, 9, 10, 11, 1]])
+    sorted_X = vector_sort(
+        bitonic_matrices(8), test_array, lambda x: (x.T @ [1, 0]).T, alpha=1.0
+    )
+    assert abs(test_array[0, 0] - -10.0) < 1.0
+    assert abs(test_array[0, -1] - 80.0) < 1.0
+    assert abs(test_array[1, 0] - 5.0) < 1.0
+    assert abs(test_array[1, -1] - 1.0) < 1.0
+
+    # check that second column not affected by small changes in first
+    # which preserve order
+    test_array_2 = np.array([[1, 2, 70, 4, 5, 6, 7, 120], [5, 6, 7, 8, 9, 10, 11, 1]])
+    assert np.allclose(test_array[1], test_array_2[1])
+
+    for n in [2, 4, 8, 16, 64, 256]:
+        matrices = bitonic_matrices(n)
+        for d in [1, 2, 3, 8, 10]:
+            for alpha in [0.01, 0.1, 1.0, 10.0]:
+                X = np.random.uniform(-100, 100, (d, n))
+                weight = np.random.uniform(0, 1, d)
+                sorted_X = vector_sort(
+                    matrices, X, lambda x: (x.T @ weight).T, alpha=alpha
+                )
+                assert sorted_X.shape == X.shape
 
 
 def test_network():
@@ -40,7 +68,7 @@ def test_argsort():
                             argsorted = diff_argsort(
                                 matrices, vec, sigma, softmax=max_fn, transpose=True
                             )
-                            
+
                             assert np.all(argsorted >= 0)
 
 
@@ -86,10 +114,10 @@ def test_matrices():
 
 def test_woven_matrices():
     for n in [2, 4, 8, 16, 32, 64, 128, 256, 512]:
-        matrices = bitonic_woven_matrices(n)        
+        matrices = bitonic_woven_matrices(n)
         for m in matrices:
-            # check shapes OK            
-            assert m.shape == (n, n)            
+            # check shapes OK
+            assert m.shape == (n, n)
             assert np.sum(m) == n
             # test valid permutation matrix
             assert np.allclose(np.sum(m, axis=0), np.ones(n))
@@ -111,7 +139,9 @@ def test_ranking():
             true_rank = rankdata(test)
             dargsort = diff_argsort(matrices, test, sigma=0.001, softmax=np.maximum)
             assert np.allclose(true_rank - 1, dargsort)
-            dargsort = diff_argsort_indexed(indices, test, sigma=0.001, softmax=np.maximum)
+            dargsort = diff_argsort_indexed(
+                indices, test, sigma=0.001, softmax=np.maximum
+            )
             assert np.allclose(true_rank - 1, dargsort)
 
 
